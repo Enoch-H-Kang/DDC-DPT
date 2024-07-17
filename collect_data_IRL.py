004a0f18f64249b17daf0e2f48b527a8dd8ea36d
Enoch H Kang <hynwk.kang@gmail.com>
2024-07-17 12:19:37 -0700

    Update collect_data_IRL.py
    One-file collect_data_IRL

--- collect_data_IRL.py
+++ collect_data_IRL.py
@@ -4,13 +4,126 @@ import pickle
 import random
 from tqdm import tqdm
 import numpy as np
-from skimage.transform import resize
-from IPython import embed
+import random
+import numpy as np
+from scipy.special import logsumexp
 
-from envs import Zurcher_env
-from utils import (
-    build_Zurcher_data_filename
-)
+
+
+class ZurcherEnv():
+    def __init__(self, theta, beta, horizon, xmax, type):
+        self.theta = theta
+        self.beta = beta
+        self.horizon = horizon
+        self.xmax = xmax
+        self.states = np.arange(self.xmax+1)
+        self.type = type #Type can be 0, 1, 2... numTypes-1
+        self.current_step = 0
+        self.EP, self.Vtil = self.calculate_EP_Vtil()
+        
+        
+    def get_util(self):
+        theta1 = self.theta[0]
+        theta2 = self.theta[1]
+        theta3 = self.theta[2]
+        u1 = -theta1 * self.states  # an array of length s with each element being -theta[0] * s
+        u2 = -theta2 * np.ones(self.states.shape)  # an array of length s with each element being -theta[1]
+        u3 = -theta3 * np.ones(self.states.shape)  # an array of length s with each element being -theta[2]
+        U = np.column_stack((u1, u2, u3))
+        # an example of a row of U is [-5, -9, -4.5], 
+        # where the first column is the cost of period maintenance at mileage 5, 
+        # the second column is the cost of replacement, and the third column is the cost of mileage
+        return U
+    
+    def vfi(self):
+        U = self.get_util()
+        gamma = np.euler_gamma
+        Vtil = np.zeros((len(self.states), 2)) #row: state, column: action
+        dist = 1
+        iter = 0
+        while dist > 1e-8:
+            V = gamma + logsumexp(Vtil, axis=1)
+            # Ensure expV corresponds to V but shifts the last element for replacement decision logic
+            expV = np.append(V[1:], V[-1])  # As mileage does not increase after it reaches max, the last element is repeated
+            Vtil1 = np.zeros_like(Vtil)  # initialize Vtil1
+            # Compute value function for maintenance (not replacing)
+            Vtil1[:, 0] = U[:, 0] + self.beta * expV  # action-specific value function of not replacing
+            # Compute value function for replacement
+            Vtil1[:, 1] = U[:, 1]+ self.type*U[:, 2] + self.beta * expV[0]  # action-specific value function of replacing
+        
+            
+            dist = np.linalg.norm(Vtil1 - Vtil)
+            Vtil = Vtil1
+            iter += 1
+
+        return Vtil, expV
+    
+    def calculate_EP_Vtil(self):
+        Vtil = self.vfi()[0]
+        EP1 = np.exp(Vtil[:, 1]) / (np.exp(Vtil[:, 0]) + np.exp(Vtil[:, 1])) 
+        EP = np.array([1-EP1, EP1]).T.tolist()
+        return EP, Vtil
+
+    def sample_state(self):
+        return np.random.randint(0, self.xmax+1)
+
+    def sample_action(self):
+        #choose either 0 or 1
+        randchoice = random.choice([[0, 1], [1, 0]])
+        return randchoice
+
+    def reset(self):
+        self.current_step = 0
+        self.state = 0
+        return self.state
+
+    def transit(self, state, action_prob):
+        #action_prob is an two-dimensional array of probs, where the first element is
+        # the prob of action 0 and the second element is the prob of action 1
+        action = np.random.choice([0, 1], p=action_prob)
+        if action == 0:
+            next_state = min(state + 1, self.xmax)
+            #reward = -self.theta[0] * state - self.type*self.theta[1]
+        elif action == 1:
+            next_state = 1
+            #reward = -self.theta[2]
+        else: 
+            raise ValueError("Invalid action")
+        self.state = next_state
+
+        return next_state, action
+
+
+    def get_obs(self):
+        return self.state.copy()
+
+    def opt_action(self, state):
+        return self.EP[state]
+
+def build_Zurcher_data_filename(env, config, mode):
+    """
+    Builds the filename for the Zurcher data.
+    Mode is either 0: train, 1: test, 2: eval.
+    """
+    filename_template = 'datasets/trajs_{}.pkl'
+    filename = env
+    if mode != 2:
+        filename += '_bustotal' + str(config['bustotal'])
+    filename += '_beta' + str(config['beta'])
+    filename += '_theta' + str(config['theta'])    
+    filename += '_numTypes' + str(config['numTypes'])    
+    filename += '_H' + str(config['horizon'])
+    filename += '_' + config['rollin_type']
+    #filename += '_extrapolation' + str(config['extrapolation'])
+    if mode == 0:
+        filename += '_train'
+    elif mode == 1:
+        filename += '_test'
+    elif mode == 2:
+        filename += '_n_eval' + str(config['n_eval'])
+        filename += '_eval'
+        
+    return filename_template.format(filename)
 
 def str_to_float_list(arg):
     return [float(x) for x in arg.strip('[]').split(',')]
@@ -36,8 +149,6 @@ def add_dataset_args(parser):
     parser.add_argument("--extrapolation", type=str, required=False,
                         default='False', help="Extrapolation")
     
-
-
 def rollin_mdp(env, rollin_type):
     states = []
     actions = []
@@ -75,10 +186,8 @@ def rollin_mdp(env, rollin_type):
 
     return states, actions, next_states
 
-
-
 def generate_Zurcher_histories(buses, theta, beta, horizon, xmax, rollin_type, **kwargs):
-    envs = [Zurcher_env.ZurcherEnv(theta, beta, horizon, xmax, busType) for busType in buses]
+    envs = [ZurcherEnv(theta, beta, horizon, xmax, busType) for busType in buses]
     
     trajs = []
     for env in tqdm(envs):
@@ -92,20 +201,12 @@ def generate_Zurcher_histories(buses, theta, beta, horizon, xmax, rollin_type, *
         query_true_EP = env.opt_action(query_state) #True optimal choice prob vector at the query state
         query_action = np.random.choice([0, 1], p=query_true_EP) #The target action chosen according to true optimal choice prob 
         query_true_Q = env.Vtil[query_state] #Q value at the query state
+
         
-        if query_action == 0:
-            one_hot_query_action = np.array([1, 0])
-        elif query_action == 1:
-            one_hot_query_action = np.array([0, 1])
-        else:
-            raise ValueError("Invalid action")
-        #one-hot encoding of query_action. For other variables, we don't use one-hot encoding.
-        #one_hot_query_action = np.zeros(2)
-        #one_hot_query_action[query_action] = 1
         
         traj = {
             'query_state': query_state,
-            'query_action': one_hot_query_action,
+            'query_action': query_action,
             'context_states': context_states,
             'context_actions': context_actions,
             'context_next_states': context_next_states,
